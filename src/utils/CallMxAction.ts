5b3e0b299c50f51692e6c3dbb58398eef39f5983
Benjamin Griggs <benjaminsgriggs@gmail.com>
2026-03-25 11:01:28 -0400

    init

--- src/utils/CallMxAction.ts
+++
@@ -1,9 +0,0 @@
-import { ActionValue } from "mendix";
-
-const callMxAction = (action: ActionValue | undefined, preventConcurrent: boolean): void => {
-    if (action !== undefined && action.canExecute && (!preventConcurrent || !action.isExecuting)) {
-        action.execute();
-    }
-};
-
-export default callMxAction;
